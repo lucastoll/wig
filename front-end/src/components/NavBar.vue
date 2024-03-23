91ee8a4139fc6a3c8f349acbe087069257accfff
RafaRosario <rafarr1702@gmail.com>
2024-03-23 12:29:03 -0300

    feat: beggining of navBar

---
+++ front-end/src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <div class ="navbar">
+      <nav>
+        <RouterLink class="routerlogo" to="/">
+        <img
+        alt="Home logo"
+        class="logo"
+        src="@/assets/Home.png"
+        width="24"
+        height="24"
+    />
+    <tittle>home</tittle>
+  </RouterLink>
+        <RouterLink class="routerlogo" to="/about">About</RouterLink>
+      </nav>
+    </div>
+</template>
+<style>
+.navbar{
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #1597B1;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 52px;
+}
+nav{
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+  color:white;
+  text-decoration: none;
+}
+.routerlogo{
+  display:flex;
+  flex-direction: column;
+  align-self: center;
+  justify-content: center;
+  color:white;
+  text-decoration: none;
+}
+</style>
