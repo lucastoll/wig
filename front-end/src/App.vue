8f96803c9e11a3ece382515944483c8fa677dfaa
RafaRosario <rafarr1702@gmail.com>
2024-03-23 12:29:03 -0300

    feat: beggining of navBar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+
+<NavBar>
+</NavBar>
+
+ 
 
   <RouterView />
 </template>
