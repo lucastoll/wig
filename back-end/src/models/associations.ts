9c6b2d7c35e3bc0736fa4b38a9325b74d0b5b768
lucastoll <lucasfernandestolotto@gmail.com>
2024-03-23 15:13:53 -0300

    feat: create tables

---
+++ back-end/src/models/associations.ts
@@ -0,0 +1,17 @@
+import { City } from "./city";
+import { User } from "./user";
+import { Category } from "./category";
+import { Event } from "./event";
+
+// User - Category
+User.belongsToMany(Category, { through: "UserCategory" });
+Category.belongsToMany(User, { through: "UserCategory" });
+// Event - User
+Event.belongsTo(User, { as: 'organizer' });
+User.hasMany(Event, { foreignKey: 'organizerId' });
+// Event - Category
+Event.belongsToMany(Category, { through: "EventCategory" });
+Category.belongsToMany(Event, { through: "EventCategory" });
+// City - Event
+City.belongsTo(Event, { as: "cityId" });
+City.hasMany(Event, { foreignKey: 'cityId' });
