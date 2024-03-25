4bd1f9588092cb5b877f1ebe9e03640e1a7b6fb8
RafaRosario <rafarr1702@gmail.com>
2024-03-25 15:57:34 -0300

    feat: inicio header

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -1,118 +1,76 @@
-<!--<template>
-    <div>
-      <button @click="openPopup">Abrir Pop-up</button>
-      <div v-if="showPopup" class="popup">
-        <div class="popup-content">
-          <p>Este é um pop-up!</p>
-          <button @click="closePopup">Fechar</button>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const showPopup = ref(false);
-  const openPopup = () => {
-    showPopup.value = true;
-  };
-  const closePopup = () => {
-    showPopup.value = false;
-  };
-  </script>
-  
-  <style>
-  .popup {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background: rgba(0, 0, 0, 0.5);
-    padding: 20px;
-  }
-  
-  .popup-content {
-    background: #fff;
-    padding: 20px;
-  }
-  </style>-->
-  
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 const showPopup = ref(false);
 const closePopup = () => {
   showPopup.value = false;
 };
 const openPopup = () => {
-    showPopup.value = true;
-  };
+  showPopup.value = true;
+};
 </script>
 <template>
-    <div class="header">
-        <img
-        alt="Logo site"
-        class="logo"
-        src="@/assets/WIG.svg"
-        width="30"
-        height="30"
-    />
-    <div class ="logomeio">
+  <div class="header">
     <img
+      alt="Logo site"
+      class="logo"
+      src="@/assets/WIG.svg"
+      width="30"
+      height="30"
+    />
+    <div class="logomeio">
+      <img
         alt="Logo site"
         class="logo"
         src="@/assets/Locais.svg"
         width="20"
         height="20"
-    />
-    <button class ="cidade" @click="openPopup" >Sorocaba</button>
-    <div v-if="showPopup" class="popup">
-    <div class="popup-content">
-        <span style="color: red">oi</span>
+      />
+      <button class="cidade" @click="openPopup">Sorocaba</button>
+      <div v-if="showPopup" class="popup">
+        <div class="popup-content">
+          <span style="color: red">oi</span>
+        </div>
+      </div>
     </div>
-    </div>
-</div>
     <img
-        alt="Logo site"
-        class="logo"
-        src="@/assets/Direita.svg"
-        width="20"
-        height="20"
+      alt="Logo site"
+      class="logo"
+      src="@/assets/Direita.svg"
+      width="20"
+      height="20"
     />
-    </div>
+  </div>
 </template>
 
 <style>
-
-.header{
-display:flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
-background-color: #1597B1;
-position: fixed;
-top: 1;
-width: 100%;
-height: 49px;
-padding-left: 16px;
-padding-right: 16px;
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1597b1;
+  position: fixed;
+  top: 1;
+  width: 100%;
+  height: 49px;
+  padding-left: 16px;
+  padding-right: 16px;
 }
-.logomeio{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap:5px;
-    color:white;
-    position: relative;
-  
+.logomeio {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  color: white;
+  position: relative;
 }
 .popup {
   position: absolute;
   bottom: -144px;
 }
 .popup-content {
-    width: 233px;
+  width: 233px;
   height: 123px;
   background: #fff;
   padding: 20px;
