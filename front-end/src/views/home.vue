312d12c87fa657aae165ac125afbb8479fb3f562
Joao <jm2matheus@gamil.com>
2024-03-27 02:27:50 -0300

    fix: image style

--- front-end/src/views/home.vue
+++ front-end/src/views/home.vue
@@ -1,121 +1,78 @@
 <template>
-  <div class="swiper-container">
-    <div class="swiper-wrapper">
-      <div v-for="(item, index) in items" :key="index" class="swiper-slide">
-        <div class="slide-content">
-          <img :src="item.image" :alt="item.alt" class="slide-image" />
-          <div class="overlay-text-container">
-            <img alt="Calendar" class="calendar" src="@/assets/Calendario.png" />
-            <span :class="'overlay-text overlay-text1-' + index">{{ item.overlayText1 }}</span>
-          </div>
-          <span :class="'overlay-text overlay-text2-' + index">{{ item.overlayText2 }}</span>
+  <div id="app">
+    <div v-if="events.length > 0" class="main-event">
+      <img :src="events[0].image" :alt="'Event Image ' + events[0].name" class="main-image">
+      <div class="details">
+        <img src="@/assets/Calendario.png" alt="Calendar Icon">
+        <div class="event-date">
+          <span>{{ events[0].date }}</span>
         </div>
+        <p>{{ events[0].name }}</p>
       </div>
     </div>
-    <div class="swiper-pagination"></div>
   </div>
 </template>
 
-<script setup>
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
-import { ref, onMounted } from "vue";
-
-const items = ref([
-  {
-    image:
-      "https://imgs.search.brave.com/BwxKWXghgg7KWw7aGSFSJbaJ9KMishuCNF--fx-lfiU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzcz/LzU3LzEzNzM1NzY1/NDM4OGRhYWRmZGY2/OGNmMzU3NDFhYzRh/LmpwZw",
-    alt: "Alt text 1",
-    overlayText1: "Festa 1",
-    overlayText2: "Evento 1"
-  },
-  {
-    image:
-      "https://imgs.search.brave.com/BwxKWXghgg7KWw7aGSFSJbaJ9KMishuCNF--fx-lfiU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzcz/LzU3LzEzNzM1NzY1/NDM4OGRhYWRmZGY2/OGNmMzU3NDFhYzRh/LmpwZw",
-    alt: "Alt text 2",
-    overlayText1: "Festa 2",
-    overlayText2: "Evento 2"
-  },
-  {
-    image:
-      "https://imgs.search.brave.com/BwxKWXghgg7KWw7aGSFSJbaJ9KMishuCNF--fx-lfiU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzcz/LzU3LzEzNzM1NzY1/NDM4OGRhYWRmZGY2/OGNmMzU3NDFhYzRh/LmpwZw",
-    alt: "Alt text 3",
-    overlayText1: "Festa 3",
-    overlayText2: "Evento 3"
-  },
-]);
-
-onMounted(() => {
-  const swiper = new Swiper(".swiper-container", {
-    // Opções do Swiper
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-});
+<script>
+export default {
+  data() {
+    return {
+      events: [
+        { 
+          image: 'https://imgs.search.brave.com/klD5KUtMfSVTPp5Hx6uPQ8QGMWOFMXUirFuXRryvtjA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMyLmFscGhhY29k/ZXJzLmNvbS83MDkv/NzA5OTc2LnBuZw',
+          date: '27/03/2024',
+          name: 'Event 1'
+        },
+        { 
+          image: 'https://imgs.search.brave.com/klD5KUtMfSVTPp5Hx6uPQ8QGMWOFMXUirFuXRryvtjA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMyLmFscGhhY29k/ZXJzLmNvbS83MDkv/NzA5OTc2LnBuZw',
+          date: '28/03/2024',
+          name: 'Event 2'
+        },
+        { 
+          image: 'https://imgs.search.brave.com/klD5KUtMfSVTPp5Hx6uPQ8QGMWOFMXUirFuXRryvtjA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMyLmFscGhhY29k/ZXJzLmNvbS83MDkv/NzA5OTc2LnBuZw',
+          date: '29/03/2024',
+          name: 'Event 3'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped>
-.swiper-container {
+.main-event {
+  position: relative;
+}
+
+.main-image {
   width: 100%;
-  height: 100%;
+  height: 300px;
 }
 
-.swiper-slide {
-  position: relative;
+.details {
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  padding: 10px;
 }
 
-.slide-content {
-  position: relative;
+.details img {
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
 }
 
-.slide-image {
-  height: 150px;
+.event {
+  margin-top: 20px;
+}
+
+.event-date{
+  display: inline;
+  margin-top: -30px;
+}
+
+.event img {
   width: 100%;
   display: block;
-  margin: 0 auto;
-}
-
-.overlay-text-container {
-  display: flex;
-  align-items: flex-end;
-  position: absolute;
-  bottom: 45px;
-  left: 0px;
-  margin-bottom: 0px; 
-}
-
-.overlay-text {
-  display: inline-block;
-  margin-bottom: 0px;
-  padding: 0px;
-  text-align: left;
-}
-
-.overlay-text1-0,
-.overlay-text1-1,
-.overlay-text1-2 {
-  color: blue;
-  font-size: 10px;
-}
-
-.overlay-text2-0,
-.overlay-text2-1,
-.overlay-text2-2 {
-  display: flex;
-  position: relative;
-  color: blue;
-  font-size: 12px;
-  left: 10px;
-  bottom: 25px;
-}
-
-.calendar {
-  height: 20px;
-  width: 20px; 
-  margin-right: 5px;
-  margin-left: 10px;
 }
 </style>
