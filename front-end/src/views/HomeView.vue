939137b3136a6b1a7e6e117f513e0523bfe35984
Joao <jm2matheus@gamil.com>
2024-03-27 10:35:59 -0300

    fix: home

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import LoginGoogle from "../components/LoginGoogle.vue";
+import Hero from "../components/Hero.vue";
 import { userStore } from "../store";
 </script>
 
 <template>
   <div>
+    <Hero />
     {{ userStore.name }}
     <LoginGoogle />
   </div>
