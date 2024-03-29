dd1e12c4853c14aad5c6aa07a26202ebf350d27f
Joao <jm2matheus@gamil.com>
2024-03-29 01:20:01 -0300

    feat: fild-nextEvents

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import NavBar from "./components/NavBar.vue";
+import NextEvents from "./components/NextEvents.vue";
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
+  <NextEvents/>
 </template>
 
 <style scoped></style>
