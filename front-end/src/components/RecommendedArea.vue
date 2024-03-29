0ab7e72492f494d05dd873975ee6cd989dd787ce
lucastoll <lucasfernandestolotto@gmail.com>
2024-03-29 19:03:26 -0300

    feat: add fetch to backend

--- front-end/src/components/RecommendedArea.vue
+++ front-end/src/components/RecommendedArea.vue
@@ -1,247 +1,283 @@
 <template>
-    <div class="recommended-events">
-      <h2 class="title">Eventos Recomendados</h2>
-      <div class="events-wrapper" ref="eventsWrapper">
-        <div class="arrow-left-container" v-if="showLeftArrow" @click="scrollLeft">
-          <div class="arrow-click-area">&#10094;</div>
-        </div>
-        <div class="event-card" v-for="(event, index) in events" :key="index" :style="{ width: eventWidth }">
-          <img :src="event.image" alt="Event Image">
-          <div class="event-details">
-            <div class="event-date">{{ event.date }}</div>
-            <div class="event-info">
-              <div class="event-name">{{ event.name }}</div>
-              <div class="event-category">{{ event.category }}</div>
-              <div class="event-location">{{ event.location }}</div>
-            </div>
+  <div class="recommended-events">
+    <h2 class="title">Eventos Recomendados</h2>
+    <div class="events-wrapper" ref="eventsWrapper">
+      <div
+        class="arrow-left-container"
+        v-if="showLeftArrow"
+        @click="scrollLeft"
+      >
+        <div class="arrow-click-area">&#10094;</div>
+      </div>
+      <div
+        class="event-card"
+        v-for="(event, index) in events"
+        :key="index"
+        :style="{ width: eventWidth }"
+      >
+        <img :src="event.image" alt="Event Image" />
+        <div class="event-details">
+          <div class="event-date">{{ event.date }}</div>
+          <div class="event-info">
+            <div class="event-name">{{ event.name }}</div>
+            <div class="event-category">{{ event.category }}</div>
+            <div class="event-location">{{ event.location }}</div>
           </div>
         </div>
-        <div class="arrow-right-container" v-if="showRightArrow" @click="scrollRight">
-          <div class="arrow-click-area">&#10095;</div>
-        </div>
+      </div>
+      <div
+        class="arrow-right-container"
+        v-if="showRightArrow"
+        @click="scrollRight"
+      >
+        <div class="arrow-click-area">&#10095;</div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        events: [
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '24 de Março',
-            name: 'Evento 1',
-            category: 'Categoria 1',
-            location: 'Localização 1'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '25 de Março',
-            name: 'Evento 2',
-            category: 'Categoria 2',
-            location: 'Localização 2'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '24 de Março',
-            name: 'Evento 1',
-            category: 'Categoria 1',
-            location: 'Localização 1'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '25 de Março',
-            name: 'Evento 2',
-            category: 'Categoria 2',
-            location: 'Localização 2'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '24 de Março',
-            name: 'Evento 1',
-            category: 'Categoria 1',
-            location: 'Localização 1'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '25 de Março',
-            name: 'Evento 2',
-            category: 'Categoria 2',
-            location: 'Localização 2'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '24 de Março',
-            name: 'Evento 1',
-            category: 'Categoria 1',
-            location: 'Localização 1'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '25 de Março',
-            name: 'Evento 2',
-            category: 'Categoria 2',
-            location: 'Localização 2'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '24 de Março',
-            name: 'Evento 1',
-            category: 'Categoria 1',
-            location: 'Localização 1'
-          },
-          {
-            image: 'https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg',
-            date: '25 de Março',
-            name: 'Evento 2',
-            category: 'Categoria 2',
-            location: 'Localização 2'
-          },
-          // Eventos aqui
-        ],
-        eventWidth: 'calc(100% - 40px)',
-        showLeftArrow: false,
-        showRightArrow: false
-      };
-    },
-    mounted() {
-      this.checkScroll();
-      window.addEventListener('resize', this.checkScroll);
-    },
-    beforeDestroy() {
-      window.removeEventListener('resize', this.checkScroll);
-    },
-    methods: {
-      checkScroll() {
-        const wrapper = this.$refs.eventsWrapper;
-        const scrollWidth = wrapper.scrollWidth;
-        const clientWidth = wrapper.clientWidth;
-        const scrollLeft = wrapper.scrollLeft;
-        const maxScroll = scrollWidth - clientWidth;
-        const lastCard = wrapper.children[wrapper.children.length - 2]; 
-  
-        const isMobile = window.innerWidth < 768;
-  
-        this.showLeftArrow = !isMobile && scrollLeft > 0;
-        this.showRightArrow = !isMobile && lastCard.offsetLeft + lastCard.clientWidth > clientWidth && scrollLeft < maxScroll;
-  
-        if (scrollWidth > clientWidth) {
-          wrapper.style.overflowX = 'scroll';
-          this.eventWidth = '';
-        } else {
-          this.eventWidth = 'calc((100% / 7) - 20px)';
-        }
-      },
-      scrollLeft() {
-        const wrapper = this.$refs.eventsWrapper;
-        wrapper.scrollBy({ left: -150, behavior: 'smooth' });
-        this.checkScroll(); 
-      },
-      scrollRight() {
-        const wrapper = this.$refs.eventsWrapper;
-        wrapper.scrollBy({ left: 150, behavior: 'smooth' });
-        this.checkScroll(); 
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .recommended-events {
-    position: relative;
-    margin-top: 40px;
-    padding-bottom: 20px;
-  }
-  
-  .title{
-    text-align: left;
-    font-size: 20px;
-    margin-left: 5px;
-    margin-right: 5px;
-    color: black;
-  }
-  
-  .events-wrapper {
-    display: flex;
-    overflow-x: hidden;
-    scrollbar-width: none;
-    -ms-overflow-style: none;
-  }
-  
-  .event-card {
-    flex: 0 0 auto;
-    margin-left: 20px;
-    width: 140px;
-    border-radius: 10px;
-    overflow: hidden; /* Garante que qualquer conteúdo que ultrapasse a borda do card seja cortado */
-    border: solid black 1px;
-  }
-  
-  .event-card img {
-    width: 100%;
-    height: 100px;
-    margin-bottom: -7px;
-  }
-  
-  
-  .event-details {
-    background-color: #1597B1;
-    color: white;
-    border-radius: 10px;
-    height: 100px
-  }
-  
-  .event-date {
-    font-weight: bold;
-    font-size: 12px;
-    background-color: #505050;
-    text-align: center;
-    margin-top: 0%;
-  }
-  
-  .event-info {
-    margin-left: 10px;
-    margin-bottom: 20px;
-  }
-  
-  .event-location,
-  .event-category{
-    font-size: 12px;
-  }
+  </div>
+</template>
 
-  .event-location{
-    margin-top: 5px;
-    margin-bottom: 5px;
-  }
-  
-  .arrow-left-container,
-  .arrow-right-container {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    cursor: pointer;
-    z-index: 1;
-  }
-  
-  .arrow-click-area {
-    width: 30px; /* Largura desejada para a área de clique */
-    height: 350px; /* Altura igual à altura do card */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    font-size: 40px;
-    color: black;
-  }
-  
-  .arrow-left-container {
-    left: 0;
-  }
-  
-  .arrow-right-container {
-    right: 0;
-  }
-  
-  </style>
-  
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      events: [
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "24 de Março",
+          name: "Evento 1",
+          category: "Categoria 1",
+          location: "Localização 1",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "25 de Março",
+          name: "Evento 2",
+          category: "Categoria 2",
+          location: "Localização 2",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "24 de Março",
+          name: "Evento 1",
+          category: "Categoria 1",
+          location: "Localização 1",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "25 de Março",
+          name: "Evento 2",
+          category: "Categoria 2",
+          location: "Localização 2",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "24 de Março",
+          name: "Evento 1",
+          category: "Categoria 1",
+          location: "Localização 1",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "25 de Março",
+          name: "Evento 2",
+          category: "Categoria 2",
+          location: "Localização 2",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "24 de Março",
+          name: "Evento 1",
+          category: "Categoria 1",
+          location: "Localização 1",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "25 de Março",
+          name: "Evento 2",
+          category: "Categoria 2",
+          location: "Localização 2",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "24 de Março",
+          name: "Evento 1",
+          category: "Categoria 1",
+          location: "Localização 1",
+        },
+        {
+          image:
+            "https://i.pinimg.com/564x/35/04/b8/3504b8d1f12c20a6885459e6585b671f.jpg",
+          date: "25 de Março",
+          name: "Evento 2",
+          category: "Categoria 2",
+          location: "Localização 2",
+        },
+        // Eventos aqui
+      ],
+      eventWidth: "calc(100% - 40px)",
+      showLeftArrow: false,
+      showRightArrow: false,
+    };
+  },
+  mounted() {
+    this.checkScroll();
+    this.fetchEvents();
+    window.addEventListener("resize", this.checkScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.checkScroll);
+  },
+  methods: {
+    async fetchEvents() {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/events?cityId=1"
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar eventos:", error);
+      }
+    },
+    checkScroll() {
+      const wrapper = this.$refs.eventsWrapper;
+      const scrollWidth = wrapper.scrollWidth;
+      const clientWidth = wrapper.clientWidth;
+      const scrollLeft = wrapper.scrollLeft;
+      const maxScroll = scrollWidth - clientWidth;
+      const lastCard = wrapper.children[wrapper.children.length - 2];
+
+      const isMobile = window.innerWidth < 768;
+
+      this.showLeftArrow = !isMobile && scrollLeft > 0;
+      this.showRightArrow =
+        !isMobile &&
+        lastCard.offsetLeft + lastCard.clientWidth > clientWidth &&
+        scrollLeft < maxScroll;
+
+      if (scrollWidth > clientWidth) {
+        wrapper.style.overflowX = "scroll";
+        this.eventWidth = "";
+      } else {
+        this.eventWidth = "calc((100% / 7) - 20px)";
+      }
+    },
+    scrollLeft() {
+      const wrapper = this.$refs.eventsWrapper;
+      wrapper.scrollBy({ left: -150, behavior: "smooth" });
+      this.checkScroll();
+    },
+    scrollRight() {
+      const wrapper = this.$refs.eventsWrapper;
+      wrapper.scrollBy({ left: 150, behavior: "smooth" });
+      this.checkScroll();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.recommended-events {
+  position: relative;
+  margin-top: 40px;
+  padding-bottom: 20px;
+}
+
+.title {
+  text-align: left;
+  font-size: 20px;
+  margin-left: 5px;
+  margin-right: 5px;
+  color: black;
+}
+
+.events-wrapper {
+  display: flex;
+  overflow-x: hidden;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.event-card {
+  flex: 0 0 auto;
+  margin-left: 20px;
+  width: 140px;
+  border-radius: 10px;
+  overflow: hidden; /* Garante que qualquer conteúdo que ultrapasse a borda do card seja cortado */
+  border: solid black 1px;
+}
+
+.event-card img {
+  width: 100%;
+  height: 100px;
+  margin-bottom: -7px;
+}
+
+.event-details {
+  background-color: #1597b1;
+  color: white;
+  border-radius: 10px;
+  height: 100px;
+}
+
+.event-date {
+  font-weight: bold;
+  font-size: 12px;
+  background-color: #505050;
+  text-align: center;
+  margin-top: 0%;
+}
+
+.event-info {
+  margin-left: 10px;
+  margin-bottom: 20px;
+}
+
+.event-location,
+.event-category {
+  font-size: 12px;
+}
+
+.event-location {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.arrow-left-container,
+.arrow-right-container {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  z-index: 1;
+}
+
+.arrow-click-area {
+  width: 30px; /* Largura desejada para a área de clique */
+  height: 350px; /* Altura igual à altura do card */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 40px;
+  color: black;
+}
+
+.arrow-left-container {
+  left: 0;
+}
+
+.arrow-right-container {
+  right: 0;
+}
+</style>
