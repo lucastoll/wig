9556c596597c5bceb5907ccf2c6256e59177e773
lucastoll <lucasfernandestolotto@gmail.com>
2024-04-14 13:22:49 -0300

    refactor: move template logic to computeds

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,27 +1,35 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import Footer from "../components/Footer.vue";
 import Hero from "../components/Hero.vue";
 import EventList from "../components/EventList.vue";
 import { cityStore, userStore } from "@/store";
+
+const heroEndpoint = computed(
+  () => `http://localhost:3000/events?cityId=${cityStore.id}`
+);
+const eventListEndpoint = computed(() => {
+  if (userStore.loggedIn === false && cityStore.id !== null) {
+    return `http://localhost:3000/events?cityId=${cityStore.id}`;
+  } else {
+    return `http://localhost:3000/events/recommendation?cityId=${cityStore.id}&userId=${userStore.id}`;
+  }
+});
+const upcomingEventsEndpoint = computed(
+  () => `http://localhost:3000/events/date?cityId=${cityStore.id}`
+);
 </script>
 
 <template>
-  <Hero
-    v-if="cityStore && userStore"
-    :endpoint="`http://localhost:3000/events?cityId=${cityStore.id}`"
-  />
+  <Hero v-if="cityStore && userStore" :endpoint="heroEndpoint" />
   <EventList
     v-if="cityStore && userStore"
-    :endpoint="
-      userStore.loggedIn === false && cityStore.id !== null
-        ? `http://localhost:3000/events?cityId=${cityStore.id}`
-        : `http://localhost:3000/events/recommendation?cityId=${cityStore.id}&userId=${userStore.id}`
-    "
+    :endpoint="eventListEndpoint"
     title="Eventos recomendados"
   />
   <EventList
     v-if="cityStore.id !== null && userStore"
-    :endpoint="`http://localhost:3000/events/date?cityId=${cityStore.id}`"
+    :endpoint="upcomingEventsEndpoint"
     title="Próximos eventos"
   />
   <Footer />
