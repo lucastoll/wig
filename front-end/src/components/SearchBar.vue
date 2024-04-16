ad1f653a1cadf022d3deff7498c99294dc382ec6
Joao <jm2matheus@gamil.com>
2024-04-16 14:15:40 -0300

    Fix: Search bar no found

--- front-end/src/components/SearchBar.vue
+++ front-end/src/components/SearchBar.vue
@@ -233,28 +233,10 @@ margin-top: 5px;
 }
 
 </style>
-=======
-    <div class="search-container">
-      <div class="search-bar">
-        <i class="fas fa-search search-icon">a</i>
-        <input
-          type="text"
-          v-model="searchQuery"
-          @input="debouncedSearch"
-          placeholder="Pesquisar..."
-          class="search-input"
-        />
-        <i class="fas fa-filter filter-icon" @click="toggleFilters">a</i>
-      </div>
-      <div v-if="showFilters" class="filter-options">
-        <!-- Opções de filtro -->
-        <div class="filter-option">Filtro 1</div>
-        <div class="filter-option">Filtro 2</div>
-        <div class="filter-option">Filtro 3</div>
-=======
->>>>>>> 9304fd6 (fix select)
       </div>
     </div>
+=======
+>>>>>>> ed386ab (Fix: Search bar no found)
   </div>
 </template>
 
@@ -266,6 +248,7 @@ export default {
   data() {
     return {
       searchQuery: '',
+      searchResults: [],
       showFilters: false,
       selectedFilters: [],
       filterOptions: [
@@ -277,8 +260,9 @@ export default {
   },
   methods: {
     debouncedSearch: debounce(function () {
-      // Implemente aqui a lógica para atualizar a requisição após 4 segundos sem digitar
-      console.log('Atualizando requisição...');
+      // Realize a busca aqui, mas não atualize os resultados de busca diretamente
+      // Em vez disso, mantenha o texto da consulta mesmo se não houver resultados
+      this.searchResults = this.performSearch();
     }, 4000),
     toggleFilters() {
       this.showFilters = !this.showFilters;
@@ -290,25 +274,30 @@ export default {
       } else {
         this.selectedFilters.splice(filterIndex, 1);
       }
+    },
+    performSearch() {
+      // Aqui você deve implementar a lógica para realizar a busca
+      // Retorne os resultados da busca (ou uma lista vazia se não houver resultados)
+      // Por enquanto, estou retornando uma lista vazia como exemplo
+      return [];
     }
   },
 };
 </script>
-
 
 <style scoped>
 .arrow {
   content: "";
   position: absolute;
   left: auto;
-  right: 51px;
+  right: 13px;
   transform: none;
   width: 0;
   height: 0;
-  top: 102px;
-  border-left: 10px solid transparent;
-  border-right: 10px solid transparent;
-  border-bottom: 10px solid #000000;
+  margin-top: -138px;
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+  border-bottom: 8px solid #000000;
 }
 
 
@@ -317,6 +306,7 @@ export default {
   margin-top: 10px;
   justify-content: center;
   flex-direction: column;
+  align-self: center;
 }
 
 .search-bar {
@@ -354,21 +344,52 @@ margin-top: 5px;
 .title-filter {
   color: black;
   align-self: flex-start;
+  font-family: Inter;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 16px;
+  text-align: left;
+  margin-top: 5px;
 }
 
 .filter-options {
+  z-index: 20;
+  position: absolute;
   width: fit-content;
-  min-width: 233px;
+  min-width: 200px;
+  top: 110px;
   height: 130px;
   padding: 10px;
-  margin-right: 50px;
+  margin-right: 40px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-self: flex-end;
   align-items: flex-start;
   border: 1px solid #000000;
+  background-color: white;
   border-radius: 4px;
+}
+
+@media screen and (min-width: 1024px) {
+  .filter-options {
+    z-index: 20;
+    position: absolute;
+    width: fit-content;
+    min-width: 233px;
+    top: 140px;
+    height: 130px;
+    padding: 10px;
+    margin-right: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-self: flex-end;
+    align-items: flex-start;
+    border: 1px solid #000000;
+    background-color: white;
+    border-radius: 4px;
+  }  
 }
 
 .filter-option {
@@ -378,6 +399,11 @@ margin-top: 5px;
   align-self: flex-start;
   color: black;
   position: relative;
+  font-family: Inter;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 14px;
+  margin-top: 8px;
 }
 
 .selected-filter-image {
@@ -389,6 +415,39 @@ margin-top: 5px;
   top: 50%;
   transform: translateY(-50%);
 }
+
+.search-results {
+  color: black;
+  align-self: left;
+  margin-left: 42px;
+  font-family: Inter;
+  font-size: 16px;
+  font-weight: 400;
+  margin-top: 10px;
+}
+
+.no-results {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.no-results-image {
+  width: 80%;
+  height: auto;
+}
+
+.no-results-text {
+  margin-top: 10px;
+  text-align: center;
+  font-family: Inter;
+  font-size: 18px;
+  padding-left: 5px;
+  padding-right: 5px;
+  color: black;
+}
+
 </style>
 <<<<<<< HEAD
   
