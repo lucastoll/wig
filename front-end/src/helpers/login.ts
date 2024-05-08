40c0d23e86bafbf251be723bd8d58d6dace07bbe
Joao <jm2matheus@gamil.com>
2024-05-08 13:42:10 -0300

    feach: notifications login

--- front-end/src/helpers/login.ts
+++ front-end/src/helpers/login.ts
@@ -2,6 +2,7 @@ import { userStore } from "@/store";
 import axios from "axios";
 import { decodeCredential, type CallbackTypes } from "vue3-google-login";
 import { logout } from "./logout";
+import { useNotification } from "@kyvg/vue3-notification";
 
 interface UserData {
   name?: string;
@@ -11,17 +12,26 @@ interface UserData {
 
 export const login: CallbackTypes.CredentialCallback = async (response) => {
   const userData: UserData = decodeCredential(response.credential);
+  const { notify } = useNotification();
   localStorage.setItem("credential", response.credential);
 
   try {
     const backEndUser = await axios.get(
       `http://localhost:3000/user/${userData.email}`
     );
+    notify({
+      title: "Usuario logado com sucesso!",
+      type: "success",
+    });
     userStore.loading = true;
     userStore.id = backEndUser.data.id;
     userStore.registerDone = true;
     userStore.Categories = backEndUser.data.Categories;
   } catch (error: any) {
+    notify({
+      title: "Erro ao realizar o login",
+      type: "error",
+    });
     if (error.response && error.response.status === 404) {
       userStore.registerDone = false;
     } else {
--- front-end/src/helpers/logout.ts
+++ front-end/src/helpers/logout.ts
@@ -1,12 +1,16 @@
 import { userStore } from "@/store";
-
+import { useNotification } from "@kyvg/vue3-notification";
 export const logout = () => {
+  const { notify } = useNotification();
   localStorage.removeItem("credential");
   userStore.loggedIn = false;
   userStore.name = "";
   userStore.email = "";
   userStore.profilePicture = "";
   userStore.loading = false;
-
+  notify({
+    title: "Sessão encerrada com sucesso!",
+    type: "success",
+  });
   delete userStore.registerDone;
 };
